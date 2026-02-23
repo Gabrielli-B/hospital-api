2788343b203bc523cfbf6990f140f53bd1b990d6
Gabrielli <gabrielliborba42@gmail.com>
2026-02-23 09:13:49 -0300

    feat: criar repositório PacienteRepository

---
+++ src/main/java/com/gabrielli/hospital_api/repository/PacienteRepository.java
@@ -0,0 +1,8 @@
+package com.gabrielli.hospital_api.repository;
+
+import com.gabrielli.hospital_api.model.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PacienteRepository extends JpaRepository<Paciente,Long> {
+
+}
